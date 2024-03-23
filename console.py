7d3e23d5504ad69c8b7d0193581ea32879be4fe8
Doreen970 <doreenwanyama20@gmail.com>
2024-03-23 12:41:36 -0700

    made console.py pycodestyle compliant

--- console.py
+++ console.py
@@ -4,8 +4,8 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter for HBNB project"""
-    
-    prompt = "(hbnb) "
+
+    prompt = "(hbnb)"
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -23,6 +23,7 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing on an empty line"""
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
